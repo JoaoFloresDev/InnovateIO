db4fde5d555776343f5222688710a8fe2d874c08
Joao Victor Flores <j199818@dac.unicamp.br>
2020-04-07 15:57:46 -0300

    Aviso se deseja salvar alterações
    Quando se coloca nova meta ou peso

--- App/App/Controllers/Profile/EditDataViewController.swift
+++ App/App/Controllers/Profile/EditDataViewController.swift
@@ -13,19 +13,41 @@ class EditDataViewController: ViewController {
     //    MARK: - IBOutlets
     
     @IBOutlet weak var weightTextField: UITextField!
-    @IBOutlet weak var PlainingTextView: UITextView!
+    @IBOutlet weak var plainingTextView: UITextView!
     
     //    MARK: - IBAction
     
     @IBAction func closeViewController(_ sender: Any) {
-        
-        self.dismiss(animated: true, completion: nil)
+        closeView()
     }
     
-        //    MARK: - Life Cicle
+    //    MARK: - Life Cicle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        plainingTextView.layer.cornerRadius = 10
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
         
+        closeView()
+
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    //    MARK: - UIAlerts
+    func closeView() {
+        
+        let refreshAlert = UIAlertController(title: "Deseja salvar alterações?", message: nil, preferredStyle: UIAlertController.Style.alert)
+        
+        refreshAlert.addAction(UIAlertAction(title: "Cancelar", style: .destructive, handler: { (action: UIAlertAction!) in
+            self.dismiss(animated: true, completion: nil)
+        }))
+        refreshAlert.addAction(UIAlertAction(title: "Salvar", style: .default, handler: { (action: UIAlertAction!) in
+            //            self.atualizeNames()
+            self.dismiss(animated: true, completion: nil)
+        }))
+        
+        present(refreshAlert, animated: true, completion: nil)
     }
 }
