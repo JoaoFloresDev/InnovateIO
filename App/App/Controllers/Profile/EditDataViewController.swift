91b2155d4ebc476f9ba51369ff7aa9a66deefd3a
Joao Victor Flores <j199818@dac.unicamp.br>
2020-04-07 15:33:13 -0300

    Tela de editar metas e nome

---
+++ App/App/Controllers/Profile/EditDataViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EditDataViewController.swift
+//  App
+//
+//  Created by Joao Flores on 07/04/20.
+//  Copyright © 2020 Joao Flores. All rights reserved.
+//
+
+import UIKit
+
+class EditDataViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
