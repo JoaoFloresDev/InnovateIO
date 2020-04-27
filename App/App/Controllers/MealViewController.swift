ea3c38c93b33ab048bd7e3ab3c79b62ca0b47dc7
Priscila <pri.zucato@gmail.com>
2020-04-27 10:37:10 -0300

    Core Data integration
    - Implemented saveMeal function to be called when "Adicionar" button is tapped.
    - Implemented updateDailyData function to be called when any changes are made to daily data (?).
    - Implemented fetchDailyData function to fetch last saved data for today when screen is shown.

--- App/App/Controllers/MealViewController.swift
+++ App/App/Controllers/MealViewController.swift
@@ -9,15 +9,57 @@
 import UIKit
 
 class MealViewController: UIViewController {
-	@IBOutlet var tableView: UITableView!
+	@IBOutlet weak var tableView: UITableView!
 	let options = ["Refeição", "Exercício", "Água"]
+    
+    var dataHandler: DataHandler?
+    var dailyDiary: DailyDiary?
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		tableView.delegate = self
 		tableView.dataSource = self
+        
+        do {
+            dataHandler = try DataHandler.getShared()
+        } catch {
+            print("Couldn't get shared DataHandler.")
+        }
+        
+        fetchDailyData()
 	}
+    
+    func saveMeal(quality: Int, hour: Int, minute: Int) {
+        do {
+            try dataHandler?.createMeal(quality: quality, hour: hour, minute: minute)
+        } catch {
+            print("Couldn't create new meal.")
+        }
+    }
+    
+    func updateDailyData() {
+        guard let dailyDiary = self.dailyDiary else { return }
+        do {
+            try dataHandler?.createDailyDiary(quality: Int(dailyDiary.quality),
+                                              didDrinkWater: dailyDiary.didDrinkWater,
+                                              didPracticeExercise: dailyDiary.didPracticeExercise,
+                                              didEatFruit: dailyDiary.didEatFruit)
+        } catch {
+            print("Couldn't update daily diary.")
+        }
+    }
+    
+    func fetchDailyData() {
+        do {
+            let date = Date()
+            let (year, month, day, _, _, _) = try date.getAllInformations()
+            dailyDiary = try dataHandler?.loadDailyDiary(year: year, month: month, day: day)
+        } catch {
+            print("There's still no daily data for today or something went wrong when trying to fetch.")
+        }
+    }
 }
+
 extension MealViewController:UITableViewDataSource, UITableViewDelegate{
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return 3
