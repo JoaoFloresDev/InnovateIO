783d270f120c60a219ad666b4d53a501235591e9
Priscila <pri.zucato@gmail.com>
2020-05-05 10:22:17 -0300

    [IO-157] Xib for meal history table view cell.

---
+++ App/App/Views/Custom Views/Meal History Cell/MealHistoryTableViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  MealHistoryTableViewCell.swift
+//  App
+//
+//  Created by Priscila Zucato on 05/05/20.
+//  Copyright © 2020 Joao Flores. All rights reserved.
+//
+
+import UIKit
+
+class MealHistoryTableViewCell: UITableViewCell {
+    @IBOutlet weak var mealImage: UIImageView!
+    @IBOutlet weak var noteLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var colorView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func setup(meal: Meal) {
+        guard let rating = Rating(rawValue: Int(meal.quality)) else {
+            print("Error when mapping quality of fetched meal to Rating instance.")
+            return
+        }
+        
+        // TODO: change to gradient
+        colorView.backgroundColor = rating.color
+        
+        // TODO: uncomment when meal note is implemented in Core Data.
+//        noteLabel.text = note == nil ? rating.defaultNoteForMeal : note
+        
+        // TODO: uncomment when meal image is implemented in Core Data.
+//        mealImage.isHidden = image == nil
+//        mealImage.image = image
+        
+        timeLabel.text = String(format: "%02d", meal.hour) + "h" + String(format: "%02d", meal.minute
+        )
+    }
+}
