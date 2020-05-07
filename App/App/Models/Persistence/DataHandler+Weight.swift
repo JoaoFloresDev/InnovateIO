67ddae6495bf80278672186037d4a524d098986f
Vinícius Hiroshi Higa <vinnyhiga137@hotmail.com>
2020-05-07 08:14:47 -0300

    Create DataHandler+Weight.swift

---
+++ App/App/Models/Persistence/DataHandler+Weight.swift
@@ -0,0 +1,13 @@
+//
+//  DataHandler+Weight.swift
+//  App
+//
+//  Created by Vinicius Hiroshi Higa on 07/05/20.
+//  Copyright © 2020 Joao Flores. All rights reserved.
+//
+
+import Foundation
+
+extension DataHandler {
+    
+}
