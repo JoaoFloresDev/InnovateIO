598a565548ff6593e68ed9c26eaf7d358e30440b
Vinícius Hiroshi Higa <vinnyhiga137@hotmail.com>
2020-05-11 12:36:41 -0300

    Now it's possible to load multiple weights from a given month

--- App/AppTests/WeightTests.swift
+++ App/AppTests/WeightTests.swift
@@ -48,10 +48,62 @@ class WeightTests: XCTestCase {
         
     }
     
+    func testMultipleData() {
+        do {
+            
+            try self.createMultipleData()
+            let weights = try self.dataHandler?.loadWeight(year: 2020, month: 5)
+            
+            print("\n\n\n ------------- \n\n\n")
+            print(weights?[0].day)
+            print(weights?[1].day)
+            
+            print("\n\n\n ------------- \n\n\n")
+            
+        }
+        catch {
+            XCTFail()
+        }
+    }
+    
     func create() throws {
         
         do {
             try self.dataHandler?.createWeight(value: 65, date: nil)
+        }
+        catch {
+            throw error
+        }
+        
+    }
+    
+    func createMultipleData() throws {
+        
+        do {
+            
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy/MM/dd HH:mm"
+            
+            let date1 = formatter.date(from: "2020/05/10 12:00")
+            let date2 = formatter.date(from: "2020/05/09 12:00")
+            let date3 = formatter.date(from: "2020/05/11 12:00")
+            
+            try self.dataHandler?.createWeight(value: 65, date: date1)
+            try self.dataHandler?.createWeight(value: 65, date: date2)
+            try self.dataHandler?.createWeight(value: 65, date: date3)
+        }
+        catch {
+            throw error
+        }
+        
+    }
+    
+    func loadMultipleData() throws {
+        
+        do {
+            
+            let weights = try self.loadMultipleData()
+            
         }
         catch {
             throw error
