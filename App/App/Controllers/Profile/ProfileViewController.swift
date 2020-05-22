b94aa9edd75b2382d190132b0caf93a250de0809
Vinícius Hiroshi Higa <vinnyhiga137@hotmail.com>
2020-05-22 08:49:22 -0300

    Core Data integrated with the Horizontal Bar

--- App/App/Controllers/Profile/ProfileViewController.swift
+++ App/App/Controllers/Profile/ProfileViewController.swift
@@ -138,9 +138,12 @@ class ProfileViewController: UIViewController, UINavigationControllerDelegate, U
             // Starting to populate and draw the charts...
             var numbersArray = [[Int32]]()
             
-            plotter.plotGraphicHorizontalBars (view: meatsGraphicBarsView, greenPercent: 0.5, yellowPercent: 0.3 )
             
-            plotter.plotGraphicHorizontalBars (view: meatsGraphicBarsView, greenPercent: 0.5, yellowPercent: 0.3 )
+            // Populating the habits on the horizontal chart bar
+            let (greenPercentage, yellowPercentage) = try plotter.loadHabitsAsPercentage()
+            plotter.plotGraphicHorizontalBars (view: meatsGraphicBarsView, greenPercent: greenPercentage, yellowPercent: yellowPercentage )
+            
+            
             
             // Populating with the weights marked on this current week
             numbersArray = try plotter.loadWeights()
@@ -150,6 +153,9 @@ class ProfileViewController: UIViewController, UINavigationControllerDelegate, U
             let colorWater = UIColor(named: "habitsWaterColor")!
             let colorFruits = UIColor(named: "habitsFruitsColor")!
             let colorExercice = UIColor(named: "habitsExerciceColor")!
+            
+            
+            
             //  Populating the habits with core data values
             numbersArray = try plotter.loadHabits()
 
