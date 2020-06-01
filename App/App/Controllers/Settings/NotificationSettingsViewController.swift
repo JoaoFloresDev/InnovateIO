cda2bfa18d85603d16c0e08fbd85c03ec48e819b
Priscila <pri.zucato@gmail.com>
2020-06-01 11:02:43 -0300

    Starting code on NotificationSettingsViewController.

--- App/App/Controllers/Settings/NotificationSettingsViewController.swift
+++ App/App/Controllers/Settings/NotificationSettingsViewController.swift
@@ -11,28 +11,65 @@ import UIKit
 class NotificationSettingsViewController: UIViewController {
     @IBOutlet weak var notificationsTableView: UITableView!
     
+    let dataSource: [NotificationSettingsHeaders] = [.meals, .weeklyUpdate]
+    
+// MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        setupTableView()
+        setupTableView()
     }
     
-//    func setupTableView() {
-//        notificationsTableView.delegate = self
-//        notificationsTableView.dataSource = self
-//    }
+// MARK: - Methods
+    func setupTableView() {
+        notificationsTableView.delegate = self
+        notificationsTableView.dataSource = self
+    }
 }
 
-//extension NotificationSettingsViewController: UITableViewDelegate, UITableViewDataSource {
-//    func numberOfSections(in tableView: UITableView) -> Int {
-//        return 2
-//    }
-//
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//
-//    }
-//}
+// MARK: - Table View Delegate and Data Source
+extension NotificationSettingsViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return dataSource.count
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let section = dataSource[section]
+        return section.notifications.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // TODO
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let section = dataSource[section]
+        return section.title
+    }
+}
+
+// MARK: - Enums to serve as data source for settings table view.
+enum NotificationSettingsHeaders {
+    case meals
+    case weeklyUpdate
+    
+    // TODO: definir os títulos
+    var title: String {
+        switch self {
+        case .meals:
+            return "Título meal"
+        case .weeklyUpdate:
+            return "Título update"
+        }
+    }
+    
+    var notifications: [NotificationType] {
+        switch self {
+        case .meals:
+            return [.addMealDinner, .addMealLunch]
+        case .weeklyUpdate:
+            return [.weaklyUpdate]
+        }
+    }
+}
