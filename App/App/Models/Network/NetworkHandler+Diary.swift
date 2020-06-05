712417ee51d2bc6e095b54baf62c1af74d279843
Vinícius Hiroshi Higa <vinnyhiga137@hotmail.com>
2020-06-05 08:46:24 -0300

    Extension created

---
+++ App/App/Models/Network/NetworkHandler+Diary.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkHandler+Diary.swift
+//  App
+//
+//  Created by Vinicius Hiroshi Higa on 04/06/20.
+//  Copyright © 2020 Joao Flores. All rights reserved.
+//
+
+import Foundation
+
+extension NetworkHandler {
+    
+    
+    
+}
