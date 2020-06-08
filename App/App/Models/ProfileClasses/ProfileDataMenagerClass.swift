1263eababf89cb7ee20c0e914c5ed7bc4c081ff7
Joao Victor Flores <j199818@dac.unicamp.br>
2020-06-08 10:28:59 -0300

    Apresentação

--- App/App/Models/ProfileClasses/ProfileDataMenagerClass.swift
+++ App/App/Models/ProfileClasses/ProfileDataMenagerClass.swift
@@ -24,7 +24,7 @@ class ProfimeDataMenager {
     }
     
     func setupHeaderInformations(goalsTextView: UITextView,currentWeightLabel: UILabel) {
-        goalsTextView.text = defaults.string(forKey: "Plain") ?? "Insira suas metas e planos no botão + \n"
+        goalsTextView.text = defaults.string(forKey: "Plain") ?? "Insira suas metas e planos no botão '+' \n"
         currentWeightLabel.text = "\(defaults.string(forKey: "Weight") ?? "00") Kg"
     }
     
